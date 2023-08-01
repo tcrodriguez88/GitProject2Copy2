35c9ca9f6d5f6a4552035d33e11d5dca47b290e8
teresa <teresa@teresas-air.lan>
2023-07-31 20:46:11 -0400

    implememted add()

--- src/demo2/DemoClass2.java
+++ src/demo2/DemoClass2.java
@@ -5,5 +5,12 @@ public class DemoClass2 {
 	public static void main (String args []) {
 		System.out.println("Test..");
 	}
+	
+	//add method
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+
+			
+	}
 
 }

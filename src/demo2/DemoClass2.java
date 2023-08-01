186710ae06d0cc945c911a625b3b5b2c8575122f
teresa <teresa@teresas-air.lan>
2023-07-31 21:25:29 -0400

    created add()2

--- src/demo2/DemoClass2.java
+++ src/demo2/DemoClass2.java
@@ -27,5 +27,11 @@ public class DemoClass2 {
 	public static void add(int a, int b, int c) {
 		System.out.println(a+b+c);
 	}
-
+	
+	//add method
+		public static void add(double a, double b ) {
+			System.out.println(a+b);
+		}
+	
+	
 }
